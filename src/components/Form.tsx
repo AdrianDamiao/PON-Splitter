8b3af03cde35692b5f5faac3b04fef434bc44505
Luan <j0hn04v@gmail.com>
2023-06-01 00:46:59 -0300

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -84,6 +84,7 @@ export function Form() {
                         <div className="mt-2">
                             <input
                                 type="number"
+                                step=".01"
                                 placeholder="0.00"
                                 className="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 {...register("transmission", {
@@ -106,6 +107,7 @@ export function Form() {
                         <div className="mt-2">
                             <input
                                 type="number"
+                                step=".01"
                                 placeholder="0.00"
                                 className="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 {...register("attenuation", {
@@ -127,6 +129,7 @@ export function Form() {
                         <div className="relative mt-2 rounded-md shadow-sm">
                             <input
                                 type="number"
+                                step=".01"
                                 placeholder="0.00"
                                 className="block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 {...register("distance", {
@@ -161,6 +164,7 @@ export function Form() {
                         <div className="mt-2">
                             <input
                                 type="number"
+                                step=".01"
                                 placeholder="0.00"
                                 className="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
                                 {...register("reception", {
