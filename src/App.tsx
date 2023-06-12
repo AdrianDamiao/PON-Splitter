5e75905a6140bd3fde1bdf9afee97000dedb5271
Luan <j0hn04v@gmail.com>
2023-06-12 13:58:49 -0300

    add bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 import { Form } from "./components/Form";
 
 function App() {
     return (
-        <>
+        <div className="bg-gray-50 h-screen">
             <Form />
-        </>
+        </div>
     );
 }
 
